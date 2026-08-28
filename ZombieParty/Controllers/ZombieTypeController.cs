7208c719266231b198c59981b3e50d3c476600b5
SlimePuppy <slimepuppy.git@gmail.com>
2026-08-27 22:48:29 -0400

    FCT ZombieType Create et Validation
    Ajout de la validation côté client et côté serveur pour ZombieType.

--- ZombieParty/Controllers/ZombieTypeController.cs
+++ ZombieParty/Controllers/ZombieTypeController.cs
@@ -8,18 +8,29 @@ namespace ZombieParty.Controllers
         // GET: ZombieTypeController
         public ActionResult Index()
         {
-this.ViewBag.MaListe = new List<ZombieType>()
-    {
-   	     new ZombieType(){TypeName= "Virus", Id=1},
-        new ZombieType(){TypeName= "Contact", Id=2}
-    };
-            return View();
+		this.ViewBag.MaListe = new List<ZombieType>()
+    		{
+   	     		new ZombieType(){TypeName= "Virus", Id=1},
+        		new ZombieType(){TypeName= "Contact", Id=2}
+    		};
+        	return View();
         }
 
 	public IActionResult Create()
-{
-    return View();
-}
+	{
+    		return View();
+	}
+
+	[HttpPost]
+	public IActionResult Create(Models.ZombieType zombieType)
+	{
+		if (ModelState.IsValid)
+		{
+			// ajouter a db
+		}
+
+		return this.View(zombieType);
+	}
 
     }
 }
